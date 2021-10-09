2d45c9d4462968874a69d2983a93fee4a08372ad
alberchec <alberchec@hotmail.com>
2021-10-08 22:30:59 -0300

    changes transmission-finish

--- transmission-finish.sh
+++ transmission-finish.sh
@@ -9,7 +9,8 @@ do
 		printf "\nTransmission has finished!\n"
 		break
 	else
-		cat /home/janci/Downloads/logTorr | tail -1
+		cat /home/janci/Downloads/logTorr | grep Progress | tail -1
+		printf "\n"
 	fi
 
 done
