9a1129c1efc2a5f735654163faef855981c297c2
alberchec <alberchec@hotmail.com>
2021-10-08 23:55:26 -0300

    full cmds to download

--- transmission-finish.sh
+++ transmission-finish.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
+transmission-cli $(cat $1) -w /home/janci/Downloads/ &> /home/janci/Downloads/logTorr &
+
 while :
 do
 	sleep 30
 
 	if cat /home/janci/Downloads/logTorr | grep -q Complete
 	then
-		printf "\nTransmission has finished!\n"
+		printf "\nTransmission Completed!\n"
+		pgrep -f transmission-cli | xargs kill
+		rm /home/janci/.config/transmission/torrents/*
+		rm /home/janci/.config/transmission/resume/*
 		break
 	else
 		cat /home/janci/Downloads/logTorr | grep Progress | tail -1
