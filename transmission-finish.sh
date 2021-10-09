d7b668bc3f9d71fbb9db57bc3bb8b39cc6f8b7c1
alberchec <alberchec@hotmail.com>
2021-10-08 22:14:24 -0300

    changes transmission-finish

--- transmission-finish.sh
+++ transmission-finish.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-if cat /home/janci/Downloads/logTorr | grep -q Complete
-then
-	printf "\nTransmission has finished!\n"
-	printf "Transmission has finished!\n"
-	printf "Transmission has finished!\n"
-	printf "Transmission has finished!\n"
-	printf "Transmission has finished!\n"
-	printf "Transmission has finished!\n"
-	printf "Transmission has finished!\n"
-else
-	cat /home/janci/Downloads/logTorr | tail -1 > /dev/tty1
-	#printf "Not finished\n" > /dev/tty1
-fi
+while :
+do
+	sleep 30
+
+	if cat /home/janci/Downloads/logTorr | grep -q Complete
+	then
+		printf "\nTransmission has finished!\n"
+		break
+	else
+		cat /home/janci/Downloads/logTorr | tail -1
+	fi
+
+done
