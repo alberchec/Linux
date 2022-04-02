a7807f04737520dfbe5c8c8fe17de5e921b177cb
alberchec <alberchec@hotmail.com>
2022-04-02 08:44:38 -0300

    change folder Downloads to downloads and change dnsleaktest source

--- torr.sh
+++ torr.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-transmission-cli $(cat $1) -w /home/alberto/Downloads/ &> /home/alberto/Downloads/logTorr &
+transmission-cli $(cat $1) -w /home/alberto/downloads/ &> /home/alberto/downloads/logTorr &
 
 while :
 do
 	sleep 30
 
-	if cat /home/alberto/Downloads/logTorr | grep -q Complete
+	if cat /home/alberto/downloads/logTorr | grep -q Complete
 	then
 		printf "\nTransmission Completed!\n"
 		pgrep -f transmission-cli | xargs kill
 		rm /home/alberto/.config/transmission/torrents/*
 		rm /home/alberto/.config/transmission/resume/*
-		rm /home/alberto/Downloads/logTorr
+		rm /home/alberto/downloads/logTorr
 		break
 	else
-		cat /home/alberto/Downloads/logTorr | grep Progress | tail -1
+		cat /home/alberto/downloads/logTorr | grep Progress | tail -1
 		printf "\n"
 	fi
 
