4e85ba2ea5a386b0852811cc55cee17b0956cde6
alberchec <alberchec@hotmail.com>
2022-04-17 07:54:27 -0300

    add env_variables file

---
+++ env_variables.cpp
@@ -0,0 +1,25 @@
+//Bash Manual Scraper to obtain list of Enviroment Variables
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+	ifstream file("bash_info.txt");
+	ofstream fileout("env_variables.txt");
+	string line;
+	while(getline(file,line) ){
+		int pos1 = line.find("Shell");
+		int pos2 = line.find("Variables");
+		if(pos1 == 3 && pos2 == 9){
+			break;
+		}
+	}
+	fileout << "Shell Variables" << endl;
+	while(getline(file,line) ){
+		if(line.find("Arrays") == 3) break;
+		fileout << line << endl;
+	}
+	file.close();
+	fileout.close();
+}
